09ecd37eaa23fa4f50d23984ee9b8a41953dd294
Mark Turner <git@markst.co.uk>
2024-07-31 23:24:25 +0930

    Add Haze dependency.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,6 +41,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.haze)
         }
     }
 }
