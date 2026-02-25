dd2fe497a0a266e1673e4fdcff8b8f795e535b06
Mark Turner <git@markst.co.uk>
2026-02-25 18:48:00 +1100

    Explicit Kotlin JVM target configuration.

--- expandable/build.gradle.kts
+++ expandable/build.gradle.kts
@@ -7,7 +7,11 @@ plugins {
 
 kotlin {
     androidTarget {
-
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
     }
 
     iosX64()
